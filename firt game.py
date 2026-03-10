5330a903257a05dacb9f1a1549b6cb71d6473ca2
njeriemmah91-ui <njeriemmah91@gmail.com>
2026-03-09 21:36:31 -0700

    Add files via upload

---
+++ firt game.py
@@ -0,0 +1,20 @@
+import random
+
+def number_guessing_game():
+    random_number = random.randint(1, 100)
+    max_attempts = 7
+
+    for attempt in range(1, max_attempts + 1):
+        guess = int(input("Enter your guess (1-100): "))
+
+        if guess < random_number:
+            print("Too low!")
+        elif guess > random_number:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the number!")
+            break
+    else:
+        print("Sorry, you ran out of attempts. The correct number was", random_number)
+
+number_guessing_game()
